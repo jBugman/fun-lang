10a06b8480a1b3d9e030d60464bcefca77672f71
Sergey Parshukov <codedby@bugman.me>
2017-06-16 05:07:01 +0300

    Moved String() implementations to printers.go

---
+++ fun/printers.go
@@ -0,0 +1,145 @@
+package fun
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func (u Undef) String() string {
+	return undefined
+}
+
+func (imp Import) String() string {
+	if imp.Alias == "" {
+		return fmt.Sprintf("import \"%s\"", imp.Path)
+	}
+	return fmt.Sprintf("import \"%s\" as \"%s\"", imp.Path, imp.Alias)
+}
+
+func (mod Module) String() string {
+	topLevels := make([]string, 1+1+len(mod.Decls))
+
+	// Module name
+	topLevels[0] = fmt.Sprintf("module %s where\n", mod.Name)
+
+	// Imports
+	imports := make([]string, len(mod.Imports))
+	for i, imp := range mod.Imports {
+		imports[i] = imp.String()
+	}
+	topLevels[1] = strings.Join(imports, lf) + lf
+
+	// Top-level declarations
+	for i, decl := range mod.Decls {
+		topLevels[2+i] = fmt.Sprint(decl)
+	}
+
+	return strings.Join(topLevels, lf) + lf
+}
+
+func (fd FuncDecl) String() string {
+	var out bytes.Buffer
+	// Type signature
+	fmt.Fprint(&out, fd.Name)
+	fmt.Fprint(&out, typeSeparator)
+	fmt.Fprint(&out, fd.Params)
+	if len(fd.Params) > 0 {
+		fmt.Fprint(&out, arrow)
+	}
+	fmt.Fprint(&out, fd.Results, lf)
+
+	// Name and parameters
+	fmt.Fprint(&out, fd.Name)
+	if len(fd.Params) > 0 {
+		fmt.Fprint(&out, space, fd.Params.Names())
+	}
+	fmt.Fprint(&out, binding)
+
+	// TODO implement body
+	if fd.Body == nil {
+		fmt.Fprint(&out, undefined)
+	} else {
+		fmt.Fprint(&out, fd.Body)
+	}
+
+	return out.String() + lf
+}
+
+func (ps Parameters) String() string {
+	ss := make([]string, len(ps))
+	for i := 0; i < len(ps); i++ {
+		ss[i] = string(ps[i].Type)
+	}
+	return strings.Join(ss, arrow)
+}
+
+// Names build parameter list for binding.
+func (ps Parameters) Names() string {
+	ss := make([]string, len(ps))
+	for i := 0; i < len(ps); i++ {
+		ss[i] = string(ps[i].Name)
+	}
+	return strings.Join(ss, space)
+}
+
+func (ts Results) String() string {
+	switch len(ts) {
+	case 0:
+		return unit
+	case 1:
+		return string(ts[0])
+	default:
+		ss := make([]string, len(ts))
+		for i := 0; i < len(ts); i++ {
+			ss[i] = string(ts[i])
+		}
+		return openBracket + strings.Join(ss, comma) + closeBracket
+	}
+}
+
+func (fa FuncApplication) String() string {
+	var buf bytes.Buffer
+	if fa.Module != "" {
+		fmt.Fprint(&buf, fa.Module, dot)
+	}
+	fmt.Fprint(&buf, fa.Name)
+	if len(fa.Arguments) > 0 {
+		args := make([]string, len(fa.Arguments))
+		for i := 0; i < len(args); i++ {
+			switch arg := fa.Arguments[i].(type) {
+			case FuncApplication:
+				args[i] = fmt.Sprintf("(%s)", arg)
+			default:
+				args[i] = fmt.Sprint(arg)
+			}
+		}
+		fmt.Fprint(&buf, space, strings.Join(args, space))
+	}
+	return buf.String()
+}
+
+// Assuming non-empty body, empty do block does not really makes sense.
+func (do DoBlock) String() string {
+	buf := bytes.NewBufferString(doDecl)
+	for _, line := range do.Text {
+		fmt.Fprint(buf, intendation, line, lf)
+	}
+	return strings.TrimSuffix(buf.String(), lf)
+}
+
+func (t String) String() string {
+	return fmt.Sprintf("%#v", t)
+}
+
+func (op InfixOperation) String() string {
+	return fmt.Sprintf("%s %s %s", op.X, op.Operator, op.Y)
+}
+
+func (t Tuple) String() string {
+	ss := make([]string, len(t))
+	for i := 0; i < len(t); i++ {
+		ss[i] = fmt.Sprint(t[i])
+	}
+	return openBracket + strings.Join(ss, comma) + closeBracket
+}
--- fun/types.go
+++ fun/types.go
@@ -1,12 +1,6 @@
 // Package fun contains Fun language AST and means to prettyprint its source code.
 package fun
 
-import (
-	"bytes"
-	"fmt"
-	"strings"
-)
-
 const (
 	lf            = "\n"
 	undefined     = "undefined"
@@ -36,7 +30,7 @@ type Import struct {
 	Alias string
 }
 
-// Decl represents declaration.
+// Decl represents top-level declaration.
 type Decl interface {
 	declMarker()
 }
@@ -58,10 +52,6 @@ type Undef bool
 // Undefined is an Undef singleton.
 const Undefined Undef = true
 
-func (u Undef) String() string {
-	return undefined
-}
-
 // FuncDecl represents function declaration.
 type FuncDecl struct {
 	Name    string
@@ -79,99 +69,11 @@ type Parameter struct {
 // Type represents type.
 type Type string
 
-func (imp Import) String() string {
-	if imp.Alias == "" {
-		return fmt.Sprintf("import \"%s\"", imp.Path)
-	}
-	return fmt.Sprintf("import \"%s\" as \"%s\"", imp.Path, imp.Alias)
-}
-
-func (mod Module) String() string {
-	topLevels := make([]string, 1+1+len(mod.Decls))
-
-	// Module name
-	topLevels[0] = fmt.Sprintf("module %s where\n", mod.Name)
-
-	// Imports
-	imports := make([]string, len(mod.Imports))
-	for i, imp := range mod.Imports {
-		imports[i] = imp.String()
-	}
-	topLevels[1] = strings.Join(imports, lf) + lf
-
-	// Top-level declarations
-	for i, decl := range mod.Decls {
-		topLevels[2+i] = fmt.Sprint(decl)
-	}
-
-	return strings.Join(topLevels, lf) + lf
-}
-
-func (fd FuncDecl) String() string {
-	var out bytes.Buffer
-	// Type signature
-	fmt.Fprint(&out, fd.Name)
-	fmt.Fprint(&out, typeSeparator)
-	fmt.Fprint(&out, fd.Params)
-	if len(fd.Params) > 0 {
-		fmt.Fprint(&out, arrow)
-	}
-	fmt.Fprint(&out, fd.Results, lf)
-
-	// Name and parameters
-	fmt.Fprint(&out, fd.Name)
-	if len(fd.Params) > 0 {
-		fmt.Fprint(&out, space, fd.Params.Names())
-	}
-	fmt.Fprint(&out, binding)
-
-	// TODO implement body
-	if fd.Body == nil {
-		fmt.Fprint(&out, undefined)
-	} else {
-		fmt.Fprint(&out, fd.Body)
-	}
-
-	return out.String() + lf
-}
-
 // Parameters represents function parameters.
 type Parameters []Parameter // TODO not only types
 
-func (ps Parameters) String() string {
-	ss := make([]string, len(ps))
-	for i := 0; i < len(ps); i++ {
-		ss[i] = string(ps[i].Type)
-	}
-	return strings.Join(ss, arrow)
-}
-
-// Names build parameter list for binding.
-func (ps Parameters) Names() string {
-	ss := make([]string, len(ps))
-	for i := 0; i < len(ps); i++ {
-		ss[i] = string(ps[i].Name)
-	}
-	return strings.Join(ss, space)
-}
-
 // Results represents function result list.
 type Results []Type
-
-func (ts Results) String() string {
-	switch len(ts) {
-	case 0:
-		return unit
-	case 1:
-		return string(ts[0])
-	default:
-		ss := make([]string, len(ts))
-		for i := 0; i < len(ts); i++ {
-			ss[i] = string(ts[i])
-		}
-		return openBracket + strings.Join(ss, comma) + closeBracket
-	}
-}
 
 // Expression is a pure function.
 type Expression interface {
@@ -207,36 +109,6 @@ const (
 	EXPRESSION funcApplicationKind = iota
 	STATEMENT  funcApplicationKind = iota
 )
-
-func (fa FuncApplication) String() string {
-	var buf bytes.Buffer
-	if fa.Module != "" {
-		fmt.Fprint(&buf, fa.Module, dot)
-	}
-	fmt.Fprint(&buf, fa.Name)
-	if len(fa.Arguments) > 0 {
-		args := make([]string, len(fa.Arguments))
-		for i := 0; i < len(args); i++ {
-			switch arg := fa.Arguments[i].(type) {
-			case FuncApplication:
-				args[i] = fmt.Sprintf("(%s)", arg)
-			default:
-				args[i] = fmt.Sprint(arg)
-			}
-		}
-		fmt.Fprint(&buf, space, strings.Join(args, space))
-	}
-	return buf.String()
-}
-
-// Assuming non-empty body, empty do block does not really makes sense.
-func (do DoBlock) String() string {
-	buf := bytes.NewBufferString(doDecl)
-	for _, line := range do.Text {
-		fmt.Fprint(buf, intendation, line, lf)
-	}
-	return strings.TrimSuffix(buf.String(), lf)
-}
 
 // Argument represents argument to which a function is applied.
 type Argument interface {
@@ -275,10 +147,6 @@ type String string
 func (t String) literalMarker()  {}
 func (t String) argumentMarker() {}
 
-func (t String) String() string {
-	return fmt.Sprintf("%#v", t)
-}
-
 // Bool maps to Go bool.
 type Bool string
 
@@ -310,20 +178,8 @@ type InfixOperation struct {
 
 func (op InfixOperation) funcBodyMarker() {}
 
-func (op InfixOperation) String() string {
-	return fmt.Sprintf("%s %s %s", op.X, op.Operator, op.Y)
-}
-
 // Operator represents binary operator
 type Operator string
 
 // Tuple represents a group of values
 type Tuple []Expression
-
-func (t Tuple) String() string {
-	ss := make([]string, len(t))
-	for i := 0; i < len(t); i++ {
-		ss[i] = fmt.Sprint(t[i])
-	}
-	return openBracket + strings.Join(ss, comma) + closeBracket
-}

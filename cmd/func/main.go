dbab2265e8983656249e63f8f66e0756591fa743
Sergey Parshukov <codedby@bugman.me>
2017-06-20 02:46:20 +0300

    Rewritten func binary

--- cmd/func/main.go
+++ cmd/func/main.go
@@ -1,64 +1,50 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
-	"io"
-	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/jBugman/fun-lang/translate"
 )
 
-func main() {
-	// Flags
-	var infile, outfile string
-	var useStdout bool
-	parseArgs(&infile, &outfile, &useStdout)
+// Flags.
+var (
+	outfile   = flag.String("o", "", "output file")
+	useStdout = flag.Bool("s", false, "use Stdout")
+	useStdin  = flag.Bool("i", false, "use Stdin")
+)
 
-	// Read source
-	var err error
-	var source []byte
-	source, err = ioutil.ReadFile(infile) // TODO optional Stdin
-	if err != nil {
-		exit(err)
+func main() {
+	flag.Parse()
+	var infile string
+	if *useStdin {
+		infile = "out.fun"
+	} else {
+		if flag.NArg() == 0 {
+			exit("please provide source file")
+		}
+		infile = flag.Arg(0)
 	}
+	if *outfile == "" { // flag not provided
+		*outfile = strings.TrimSuffix(infile, filepath.Ext(infile)) + ".fun"
+	}
+
+	source := readInput(infile, *useStdin)
 
 	// Parse Go AST
 	tree, fset := parseGoAST(infile, source)
-	// debugPrint(tree, fset)
 
 	// Translate to Fun
-	gofun := translate.NewFun(fset)
-	module, err := gofun.Module(tree)
+	module, err := translate.NewFun(fset).Module(tree)
 	if err != nil {
 		exit(err)
 	}
 	result := fmt.Sprint(module)
 
 	// Write output
-	writeOutput(result, outfile, useStdout)
-}
-
-func debugPrint(tree *ast.File, fset *token.FileSet) string {
-	var buf bytes.Buffer
-	ast.Fprint(&buf, fset, tree, ast.NotNilFilter)
-	return buf.String()
-}
-
-func parseGoAST(filename string, source []byte) (*ast.File, *token.FileSet) {
-	fset := token.NewFileSet()
-	tree, err := parser.ParseFile(fset, filename, source, 0)
-	if err != nil {
-		exit(err)
-	}
-	return tree, fset
+	writeOutput(result, *outfile, *useStdout)
 }
 
 func parseArgs(infile, outfile *string, useStdout *bool) {
@@ -68,33 +54,4 @@ func parseArgs(infile, outfile *string, useStdout *bool) {
 	if flag.NArg() == 0 {
 		exit("please provide source file")
 	}
-	*infile = flag.Arg(0)
-	if *outfile == "" {
-		*outfile = strings.TrimSuffix(*infile, filepath.Ext(*infile)) + ".fun"
-	}
-}
-
-func writeOutput(text string, filename string, useStdout bool) {
-	var err error
-	var out io.WriteCloser
-	if useStdout {
-		out = os.Stdout
-	} else {
-		out, err = os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
-		if err != nil {
-			exit(err)
-		}
-	}
-	_, err = io.WriteString(out, text)
-	if err != nil {
-		exit(err)
-	}
-	if !useStdout {
-		out.Close()
-	}
-}
-
-func exit(err interface{}) {
-	fmt.Fprintln(os.Stderr, err)
-	os.Exit(1)
 }
---
+++ cmd/func/tools.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func parseGoAST(filename string, source []byte) (*ast.File, *token.FileSet) {
+	fset := token.NewFileSet()
+	tree, err := parser.ParseFile(fset, filename, source, 0)
+	if err != nil {
+		exit(err)
+	}
+	return tree, fset
+}
+
+func readInput(filename string, useStdin bool) []byte {
+	var err error
+	var result []byte
+	var in io.ReadCloser
+	if useStdin {
+		in = ioutil.NopCloser(os.Stdin)
+	} else {
+		in, err = os.Open(filename)
+		if err != nil {
+			exit(err)
+		}
+	}
+	defer in.Close()
+	result, err = ioutil.ReadAll(in)
+	if err != nil {
+		exit(err)
+	}
+	return result
+}
+
+func writeOutput(text string, filename string, useStdout bool) {
+	var err error
+	var out io.WriteCloser
+	if useStdout {
+		out = os.Stdout
+	} else {
+		out, err = os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			exit(err)
+		}
+	}
+	_, err = io.WriteString(out, text)
+	if err != nil {
+		exit(err)
+	}
+	if !useStdout {
+		out.Close()
+	}
+}
+
+func exit(err interface{}) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+}
+
+// debugPrint prints Go AST
+func debugPrint(tree *ast.File, fset *token.FileSet) string {
+	var buf bytes.Buffer
+	ast.Fprint(&buf, fset, tree, ast.NotNilFilter)
+	return buf.String()
+}

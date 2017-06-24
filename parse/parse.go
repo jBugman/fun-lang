ba64be9ffc2d9d5ccc5c1cbdf91f156a31e272ba
Sergey Parshukov <codedby@bugman.me>
2017-06-24 04:07:49 +0300

    Using func-parse from Go

--- parse/parse.go
+++ parse/parse.go
@@ -3,6 +3,7 @@ package parse
 
 import (
 	"bytes"
+	"os/exec"
 	"strings"
 
 	"github.com/jBugman/fun-lang/fun"
@@ -19,4 +20,13 @@ func String(source string) (*fun.Module, error) {
 func Bytes(source []byte) (*fun.Module, error) {
 	p := parser.NewParser(bytes.NewReader(source))
 	return p.Parse()
+}
+
+// Offload uses func-parse tool to parse
+func Offload(source []byte) ([]byte, error) {
+	const path = ".stack-work/install/x86_64-osx/lts-8.20/8.0.2/bin/" // temporary dev path
+	cmd := exec.Command(path + "func-parse")
+	cmd.Stdin = bytes.NewReader(source)
+	out, err := cmd.CombinedOutput()
+	return out, err
 }

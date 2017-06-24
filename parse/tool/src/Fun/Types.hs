ebc886567d03fb91c854d7ab227b7d13990c3b1a
Sergey Parshukov <codedby@bugman.me>
2017-06-24 22:50:16 +0300

    Started alternative FuncDecl parser

--- parse/tool/src/Fun/Types.hs
+++ parse/tool/src/Fun/Types.hs
@@ -1,9 +1,9 @@
 module Fun.Types where
 
-data Module = Module {
-    moduleName :: String,
-    imports    :: [Import],
-    decls      :: [Decl]
+data Package = Package {
+    packageName :: String,
+    imports     :: [Import],
+    decls       :: [Decl]
 } deriving (Eq, Show)
 
 data Import = Import {
@@ -14,13 +14,19 @@ data Import = Import {
 data Decl = FuncDecl {
     funcName :: String,
     params   :: [Type],
-    result   :: Type,
+    result   :: Result,
     args     :: [Var],
     body     :: FuncBody
 } deriving (Eq, Show)
 
-type Type = String
-
 type Var = String
+
+type TypeName = String
+
+data Type = Int | Type TypeName | Tuple [TypeName]
+    deriving (Eq, Show)
+
+data Result = JustIO | IO Type | Pure Type
+    deriving (Eq, Show)
 
 data FuncBody = Undefined deriving (Eq, Show)

86c531ea681eabbdef6028d90249326b9af0b5df
Sergey Parshukov <codedby@bugman.me>
2017-06-25 20:27:29 +0300

    Migrated to Text.Megaparsec

--- parse/tool/src/Fun/Parser.hs
+++ parse/tool/src/Fun/Parser.hs
@@ -1,22 +1,21 @@
 module Fun.Parser where
 
 import Data.List (intercalate)
-import Text.Parsec (parse, (<|>), try, ParseError)
-import Text.Parsec.Char (char)
-import Text.Parsec.Combinator (optionMaybe, sepBy1, count)
-import Text.Parsec.String (Parser)
+import Text.Megaparsec (parse, (<|>), optional, try, char, sepBy1, count)
+import Text.Megaparsec.String (Parser)
 
 import Fun.Lexer
 import qualified Fun.Types as Fun
 
-prs :: Parser a -> String -> Either ParseError a
+
+-- prs :: Parser a -> String -> Either ParseError a
 prs rule = parse rule ""
 
 funImport :: Parser Fun.Import
 funImport = do
-    reserved "import"
+    rword "import"
     path <- quoted $ joinedBy '/' $ joinedBy '.' $ joinedBy '-' identifier
-    alias <- optionMaybe (whitespace *> reserved "as" *> quoted identifier)
+    alias <- optional (sp *> rword "as" *> quoted identifier)
     return $ Fun.Import path alias
     where
         joinedBy :: Char -> Parser String -> Parser String
@@ -24,11 +23,11 @@ funImport = do
 
 funFuncDecl :: Parser Fun.Decl
 funFuncDecl = do
-    reserved "func"
+    rword "func"
     name <- identifier
     params <- try funcParams <|> return []
-    results <- try (reserved "->" *> funcResults) <|> return []
-    reservedOp "="
+    results <- try (rword "->" *> funcResults) <|> return []
+    rword "="
     body <- funcBody
     return $ Fun.FuncDecl name params results body
 
@@ -39,10 +38,10 @@ funcResults = do
 
 funcParams :: Parser [Fun.Param]
 funcParams = do
-    xs <- parensList (identifier `sepBy1` reserved "::")
+    xs <- parensList (identifier `sepBy1` rword "::")
     return $ map (\[x, y] -> Fun.Param x (Fun.Type y)) xs
 
 funcBody :: Parser Fun.FuncBody
 funcBody = do
-    reserved "undefined"
+    rword "undefined"
     return Fun.Undefined

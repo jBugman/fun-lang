bd7c983c0361392e2ff58d5f8154c60d37c0e4c3
Sergey Parshukov <codedby@bugman.me>
2017-06-29 20:23:37 +0300

    Parsing tool

--- parse/parse.go
+++ parse/parse.go
@@ -24,8 +24,7 @@ func Bytes(source []byte) (*fun.Module, error) {
 
 // Offload uses func-parse tool to parse
 func Offload(source []byte) ([]byte, error) {
-	const path = ".stack-work/install/x86_64-osx/lts-8.20/8.0.2/bin/" // temporary dev path
-	cmd := exec.Command(path + "func-parse")
+	cmd := exec.Command("func-parse")
 	cmd.Stdin = bytes.NewReader(source)
 	out, err := cmd.CombinedOutput()
 	return out, err

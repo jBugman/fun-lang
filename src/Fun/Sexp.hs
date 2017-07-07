95a600b9ae9988147684c72c57f9a2e03e6e0b15
Sergey Parshukov <codedby@bugman.me>
2017-07-08 01:34:54 +0300

    'Pretty' printing

--- src/Fun/Sexp.hs
+++ src/Fun/Sexp.hs
@@ -1,8 +1,8 @@
 module Fun.Sexp where
 
-import Data.List           (elem, isPrefixOf, length)
+import Data.List           (elem, intercalate, isPrefixOf, length)
 import Data.String         (IsString, fromString)
-import Data.Text           (Text, pack)
+import Data.Text           (Text, pack, unpack)
 import Data.Text.Buildable (Buildable, build)
 
 import qualified Data.Text.Lazy.Builder as B
@@ -15,7 +15,7 @@ data Expression
     | Type Text
     | Atom Text
     | Unit
-        deriving (Eq, Show)
+    deriving (Eq)
 
 instance IsString Expression where
     fromString s
@@ -29,6 +29,17 @@ instance Buildable Expression where
     build (Type s) = B.fromText s
     build (Op s)   = B.fromText s
     build s        = errorWithoutStackTrace $ "Can only print terminal nodes, but got " ++ (show s)
+
+instance Show Expression where
+    show Unit      = "()"
+    show (Atom s)  = unpack s
+    show (Type s)  = unpack s
+    show (Op s)    = unpack s
+    show (List xs) = "[" ++ showContents xs ++ "]"
+    show (Exp xs)  = "(" ++ showContents xs ++ ")"
+
+showContents :: [Expression] -> String
+showContents xs = intercalate " " $ map show xs -- TODO: add line-fold on long lists and some keywords
 
 opChars :: String
 opChars = "=+-*/<>%"

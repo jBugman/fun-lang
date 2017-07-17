d598d0e99f10bde84fec9fde00e52f4cf77f7b3c
Sergey Parshukov <codedby@bugman.me>
2017-07-17 23:18:50 +0300

    Cleaner swapping

--- src/Fun/Go/Desugar.hs
+++ src/Fun/Go/Desugar.hs
@@ -7,42 +7,38 @@ import Data.Traversable             (mapAccumR)
 
 import Fun.SExpression (Expression, pattern ID, pattern KW, pattern SL)
 
+type E = Expression
 
-desugar :: Expression -> Expression
-desugar (L (KW "package" : ID name : topLevels)) = L $ [ KW "package", ID name ] <> imports'' <> decls''
+desugar :: E -> E
+desugar (L (KW "package" : ID name : topLevels))
+    = L $ [ KW "package", ID name ] <> imports <> decls
     where
-        (imports'', decls'') = swapPrintf (imports', decls')
-        (imports', decls')   = swapPrint (imports, decls)
-        (imports, decls)     = span isImport topLevels
+        (imports, decls) = swapPrintf . swapPrint . span isImport $ topLevels
 
+        swapPrint  = swapAddImport importFmt (== KW "print")  (ID "fmt.Println")
+        swapPrintf = swapAddImport importFmt (== KW "printf") (ID "fmt.Printf")
+
+-- ignore non-package
 desugar e = e
 
 
-importFmt :: Expression
+importFmt :: E
 importFmt = L [ KW "import" , SL "fmt" ]
 
-isImport :: Expression -> Bool
+
+isImport :: E -> Bool
 isImport (L (KW "import" : _)) = True
 isImport _                     = False
 
-swapPrint :: ([Expression], [Expression]) -> ([Expression], [Expression])
-swapPrint (imports, decls) = mapAccumR go imports decls
+
+swapAddImport :: E -> (E -> Bool) -> E -> ([E], [E]) -> ([E], [E])
+swapAddImport imp cond to (imports, decls) = mapAccumR go imports decls
     where
-        go :: [Expression] -> Expression -> ([Expression], Expression)
-        go imps (KW "print") = (addFmt imps, ID "fmt.Println")
-        go imps (L xs)       = L <$> mapAccumR go imps xs
-        go imps ex           = (imps, ex)
+        go :: [E] -> E -> ([E], E)
+        go imps ex
+            | cond ex    = (addFmt imps, to)
+            | L xs <- ex = L <$> mapAccumR go imps xs
+            | otherwise  = (imps, ex)
 
-        addFmt :: [Expression] -> [Expression]
-        addFmt xs = ordNub $ importFmt : xs
-
-swapPrintf :: ([Expression], [Expression]) -> ([Expression], [Expression])
-swapPrintf (imports, decls) = mapAccumR go imports decls
-    where
-        go :: [Expression] -> Expression -> ([Expression], Expression)
-        go imps (KW "printf") = (addFmt imps, ID "fmt.Printf")
-        go imps (L xs)        = L <$> mapAccumR go imps xs
-        go imps ex            = (imps, ex)
-
-        addFmt :: [Expression] -> [Expression]
-        addFmt xs = ordNub $ importFmt : xs
+        addFmt :: [E] -> [E]
+        addFmt xs = ordNub $ imp : xs

837815ae33db95d145fff525b963648e8aeec7ee
Sergey Parshukov <codedby@bugman.me>
2017-08-06 17:53:48 +0300

    Unit test in Ginkgo

--- fun-parse/parser/unit_test.go
+++ fun-parse/parser/unit_test.go
@@ -1,138 +1,191 @@
 package parser
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
 	"github.com/jBugman/fun-lang/fun-parse/fun"
 	"github.com/jBugman/fun-lang/fun-parse/fun/code"
 )
 
-func ident(x string, pos code.Pos) fun.Ident {
+var _ = Describe("parseExpression", func() {
+
+	It("parses 'foo'", func() {
+		source := newScanner("foo")
+		x, s, err := parseExpression(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			ident(pos(1, 1),
+				"foo"),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 4),
+		))
+		Expect(s.cursor).To(Equal(3))
+	})
+
+	It("parses 'foo bar'", func() {
+		source := newScanner("foo bar")
+		x, s, err := parseExpression(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			ident(pos(1, 1),
+				"foo"),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 4),
+		))
+		Expect(s.cursor).To(Equal(3))
+	})
+})
+
+var _ = Describe("parseIdent", func() {
+
+	It("parses 'foo bar'", func() {
+		source := newScanner("foo bar")
+		x, s, err := parseIdent(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			ident(pos(1, 1),
+				"foo"),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 4),
+		))
+		Expect(s.cursor).To(Equal(3))
+	})
+
+	It("parses 'foo\\n\\nbar'", func() {
+		source := newScanner("foo\n\nbar")
+		x, s, err := parseIdent(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			ident(pos(1, 1),
+				"foo"),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 4),
+		))
+		Expect(s.cursor).To(Equal(3))
+	})
+})
+
+var _ = Describe("skipSpace", func() {
+
+	It("skips space", func() {
+		source := newScanner("  \n\nbar")
+		s := skipSpace(source)
+
+		Expect(s.pos).To(Equal(
+			pos(3, 1),
+		))
+		Expect(s.cursor).To(Equal(4))
+	})
+})
+
+var _ = Describe("parseList", func() {
+
+	It("()", func() {
+		source := newScanner("()")
+		x, s, err := parseList(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			list(pos(1, 1)),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 3)))
+		Expect(s.cursor).To(Equal(2))
+	})
+
+	It("(foo)", func() {
+		source := newScanner("(foo)")
+		x, s, err := parseList(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			list(pos(1, 1),
+				ident(pos(1, 2),
+					"foo"),
+			),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 6),
+		))
+		Expect(s.cursor).To(Equal(5))
+	})
+
+	It("(foo bar)", func() {
+		source := newScanner("(foo bar)")
+		x, s, err := parseList(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			list(pos(1, 1),
+				ident(pos(1, 2),
+					"foo"),
+				ident(pos(1, 6),
+					"bar"),
+			),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 10),
+		))
+		Expect(s.cursor).To(Equal(9))
+	})
+
+	It("(foo ())", func() {
+		source := newScanner("(foo ())")
+		x, s, err := parseList(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			list(pos(1, 1),
+				ident(pos(1, 2),
+					"foo"),
+				list(pos(1, 6)),
+			),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 9),
+		))
+		Expect(s.cursor).To(Equal(8))
+	})
+
+	It("(foo (a))", func() {
+		source := newScanner("(foo (a))")
+		x, s, err := parseList(source)
+
+		Expect(err).Should(Succeed())
+		Expect(x).To(Equal(
+			list(pos(1, 1),
+				ident(pos(1, 2),
+					"foo"),
+				list(pos(1, 6),
+					ident(pos(1, 7),
+						"a"),
+				),
+			),
+		))
+		Expect(s.pos).To(Equal(
+			pos(1, 10),
+		))
+		Expect(s.cursor).To(Equal(9))
+	})
+})
+
+func ident(pos code.Pos, x string) fun.Ident {
 	return fun.NewIdent(x, pos)
 }
 
-func list(xs []fun.Expr, pos code.Pos) fun.List {
+func list(pos code.Pos, xs ...fun.Expr) fun.List {
 	return fun.NewList(xs, pos)
 }
 
 func pos(line, col int) code.Pos {
 	return code.NewPos(line, col)
-}
-
-func TestParseExpression_0(t *testing.T) {
-	source := newScanner("foo")
-	x, s, err := parseExpression(source)
-	assert.NoError(t, err)
-	assert.Equal(t, ident("foo", pos(1, 1)), x)
-	assert.Equal(t, pos(1, 4), s.pos)
-	assert.Equal(t, 3, s.cursor)
-}
-
-func TestParseExpression_1(t *testing.T) {
-	source := newScanner("foo bar")
-	x, s, err := parseExpression(source)
-	assert.NoError(t, err)
-	assert.Equal(t, ident("foo", pos(1, 1)), x)
-	assert.Equal(t, pos(1, 4), s.pos)
-	assert.Equal(t, 3, s.cursor)
-}
-
-func TestParseIdent_0(t *testing.T) {
-	source := newScanner("foo bar")
-	x, s, err := parseIdent(source)
-	assert.NoError(t, err)
-	assert.Equal(t, ident("foo", pos(1, 1)), x)
-	assert.Equal(t, pos(1, 4), s.pos)
-	assert.Equal(t, 3, s.cursor)
-}
-
-func TestParseIdent_1(t *testing.T) {
-	source := newScanner("foo\n\nbar")
-	x, s, err := parseIdent(source)
-	assert.NoError(t, err)
-	assert.Equal(t, ident("foo", pos(1, 1)), x)
-	assert.Equal(t, pos(1, 4), s.pos)
-	assert.Equal(t, 3, s.cursor)
-}
-
-func TestSkipSpace(t *testing.T) {
-	source := newScanner("  \n\nbar")
-	s := skipSpace(source)
-	assert.Equal(t, pos(3, 1), s.pos)
-	assert.Equal(t, 4, s.cursor)
-}
-
-func TestParseList_0(t *testing.T) {
-	source := newScanner("(foo)")
-	x, s, err := parseList(source)
-	assert.NoError(t, err)
-	assert.Equal(t, list(
-		[]fun.Expr{
-			ident("foo", pos(1, 2)),
-		},
-		pos(1, 1),
-	), x)
-	assert.Equal(t, pos(1, 6), s.pos)
-	assert.Equal(t, 5, s.cursor)
-}
-
-func TestParseList_1(t *testing.T) {
-	source := newScanner("(foo bar)")
-	x, s, err := parseList(source)
-	assert.NoError(t, err)
-	assert.Equal(t, list(
-		[]fun.Expr{
-			ident("foo", pos(1, 2)),
-			ident("bar", pos(1, 6)),
-		},
-		pos(1, 1),
-	), x)
-	assert.Equal(t, pos(1, 10), s.pos)
-	assert.Equal(t, 9, s.cursor)
-}
-
-func TestParseList_3(t *testing.T) {
-	source := newScanner("(foo ())")
-	x, s, err := parseList(source)
-	assert.NoError(t, err)
-	assert.Equal(t, list(
-		[]fun.Expr{
-			ident("foo", pos(1, 2)),
-			list(nil, pos(1, 6)),
-		},
-		pos(1, 1),
-	), x)
-	assert.Equal(t, pos(1, 9), s.pos)
-	assert.Equal(t, 8, s.cursor)
-}
-
-func TestParseList_4(t *testing.T) {
-	source := newScanner("()")
-	x, s, err := parseList(source)
-	assert.NoError(t, err)
-	assert.Equal(t, list(nil, pos(1, 1)), x)
-	assert.Equal(t, pos(1, 3), s.pos)
-	assert.Equal(t, 2, s.cursor)
-}
-
-func TestParseList_5(t *testing.T) {
-	source := newScanner("(foo (a))")
-	x, s, err := parseList(source)
-	assert.NoError(t, err)
-	assert.Equal(t, list(
-		[]fun.Expr{
-			ident("foo", pos(1, 2)),
-			list(
-				[]fun.Expr{
-					ident("a", pos(1, 7)),
-				},
-				pos(1, 6),
-			),
-		},
-		pos(1, 1),
-	), x)
-	assert.Equal(t, pos(1, 10), s.pos)
-	assert.Equal(t, 9, s.cursor)
 }

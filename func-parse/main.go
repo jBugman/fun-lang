29e507bca01be1c0438eff097a39879486c49573
Sergey Parshukov <codedby@bugman.me>
2017-08-08 15:43:31 +0300

    JSON-wrapped errors

--- func-parse/main.go
+++ func-parse/main.go
@@ -2,33 +2,48 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 
 	"github.com/pkg/errors"
 
+	"github.com/jBugman/fun-lang/func-parse/fun/code"
 	"github.com/jBugman/fun-lang/func-parse/parser"
 )
 
 func main() {
 	source, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, errors.Wrap(err, "reading input"))
-		os.Exit(1)
+		abortWith(errors.Wrap(err, "reading stdin"))
 	}
 
 	result, e := parser.Parse(source)
 	if e != nil {
-		const filename = "<standard input>"
-		fmt.Fprintf(os.Stderr, "%s:%d:%d: %s\n", filename, e.Pos().Line, e.Pos().Col, e)
-		os.Exit(1)
+		abortWithJ(jsonError{
+			Error: e.Error(),
+			Pos:   e.Pos(),
+		})
 	}
 
 	output, err := json.MarshalIndent(result, "", "  ")
 	if err != nil {
-		fmt.Fprintln(os.Stderr, errors.Wrap(err, "marshal json"))
-		os.Exit(1)
+		abortWith(errors.Wrap(err, "marshal json"))
 	}
 	os.Stdout.Write(output)
+}
+
+type jsonError struct {
+	Error string   `json:"error"`
+	Pos   code.Pos `json:"pos,omitempty"`
+}
+
+func abortWith(err error) {
+	abortWithJ(jsonError{Error: err.Error()})
+}
+
+func abortWithJ(err jsonError) {
+	output, _ := json.MarshalIndent(err, "", "  ")
+	os.Stderr.Write(output)
+	os.Stderr.WriteString("\n")
+	os.Exit(1)
 }
